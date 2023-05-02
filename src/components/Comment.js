2f762263d8843b725b48b51492e498109dc1ec19
SamuelDellac <samueldellac@gmail.com>
2023-05-02 19:36:02 +0200

    updatee

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Navbar from '../components/Navbar';
 import './Comment.css';
 
-const CommentSection = ({ resourceId }) => {
+const CommentSection = ({ resourceId, currentUser }) => {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
@@ -15,7 +15,7 @@ const CommentSection = ({ resourceId }) => {
         setComments(response.data);
       } catch (error) {
         console.error(error);
-        setErrorMsg("Impossible d'afficher les commentaires");
+        setErrorMsg('Erreur de chargement des commentaires');
       }
     };
     fetchComments();
@@ -23,8 +23,8 @@ const CommentSection = ({ resourceId }) => {
 
   const handleCommentSubmit = async (event) => {
     event.preventDefault();
-    if (newComment.length < 10 || newComment.length > 10000) {
-      setErrorMsg('Le commentaire doit faire entre  10 et  10000 caractères ');
+    if (newComment.length < 10 || newComment.length > 7000) {
+      setErrorMsg('Le commentaire doit faire  entre 10 et 7000 caractères ');
       return;
     }
     const commentData = { content: newComment, resourceId };
@@ -34,37 +34,61 @@ const CommentSection = ({ resourceId }) => {
       setNewComment('');
     } catch (error) {
       console.error(error);
-      setErrorMsg('Ajout de commentarie échoué ');
+      setErrorMsg("Echec d'ajout du commentaire");
+    }
+  };
+
+  const handleCommentDelete = async (commentId) => {
+    const confirmDelete = window.confirm('Confirmer la suppresion?');
+    if (!confirmDelete) {
+      return;
+    }
+    try {
+      await axios.delete(`/api/comment/${commentId}`);
+      setComments(comments.filter((comment) => comment.id !== commentId));
+    } catch (error) {
+      console.error(error);
+      setErrorMsg('Suppression échoué');
     }
   };
 
   return (
     <div>
-      <h3>Commentaires </h3>
+      <h3>Comments</h3>
       {errorMsg && <p className='text-danger'>{errorMsg}</p>}
       {comments.length === 0 && <p>Aucun commentaires</p>}
       {comments.length > 0 && (
         <ul className='list-group'>
-          {comments.map(comment => (
+          {comments.map((comment) => (
             <li className='list-group-item' key={comment.id}>
               {comment.user.username} - {new Date(comment.date).toLocaleString()}<br />
               {comment.content}
+              {currentUser.role === 'Moderateur' || currentUser.role === 'Administrateur' ? (
+                <button
+                  type='button'
+                  className='btn btn-sm btn-danger float-right'
+                  onClick={() => handleCommentDelete(comment.id)}
+                >
+                  Supprimer
+                </button>
+              ) : null}
             </li>
           ))}
         </ul>
       )}
       <form onSubmit={handleCommentSubmit}>
-        <div className='form-group'>
           <textarea
             id='new-comment'
             className='form-control'
-            rows='4'
+            rows='3'
             value={newComment}
-            placeholder="Ajouter  un commentaire"
-            onChange={e => setNewComment(e.target.value)}
+            placeholder="Ajouter un commentaire"
+            onChange={(e) => setNewComment(e.target.value)}
           />
-        </div>
-        <button type='submit' className='btn btn-primary'>Ajouter</button>
+
+        <button type='submit' className='btn btn-primary'>
+          Ajouter
+        </button>
       </form>
     </div>
   );
